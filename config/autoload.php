39c132860ead8bf4b77705c075eb3e5cfafb6276
Andreas Schempp <andreas.schempp@terminal42.ch>
2012-11-09 17:20:06 +0100

    Added autoload files for Contao 3

---
+++ config/autoload.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2012
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'ChangeLanguage'               => 'system/modules/changelanguage/ChangeLanguage.php',
+	'ModuleChangeLanguage'         => 'system/modules/changelanguage/ModuleChangeLanguage.php',
+	'ModuleLanguageRedirect'       => 'system/modules/changelanguage/ModuleLanguageRedirect.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'mod_changelanguage'    => 'system/modules/changelanguage/templates',
+));
+
