a972e8851edec5dc754f55f4dc76461261ebea17
Yanick Witschi <yanick.witschi@terminal42.ch>
2015-12-07 10:09:09 +0100

    PHP7 compat

--- ModuleChangeLanguage.php
+++ ModuleChangeLanguage.php
@@ -201,7 +201,7 @@ class ModuleChangelanguage extends Module
                     foreach ($GLOBALS['TL_HOOKS']['translateUrlParameters'] as $callback)
                     {
                         $this->import($callback[0]);
-                        $arrParams = $this->$callback[0]->$callback[1](
+                        $arrParams = $this->{$callback[0]}->{$callback[1]}(
                             $arrParams,
                             $arrRootPage['language'],
                             $arrRootPage,
