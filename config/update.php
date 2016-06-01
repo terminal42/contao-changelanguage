269d95d8667397047c6db437c9e67fbfd6d7c554
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-06-01 10:57:32 +0200

    Runonce exceptions are catched and not visible to the user

--- config/update.php
+++ config/update.php
@@ -1,5 +1,5 @@
 <?php
 
 if (0 !== count(array_intersect(ModuleLoader::getActive(), array('newslanguage', 'calendarlanguage')))) {
-    throw new LogicException('ChangeLanguage v3 is not compatible with newslanguage and calendarlanguage. Please remove system/modules/newslanguage and/or system/modules/calendarlanguage');
+    die('changelanguage v3 is not compatible with newslanguage and calendarlanguage. Please remove system/modules/newslanguage and/or system/modules/calendarlanguage');
 }
