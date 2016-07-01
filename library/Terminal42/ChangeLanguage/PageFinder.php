a182d700c293f2141b477b5f8c17fbe24657f21d
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-07-01 13:25:41 +0200

    Added new page finder class with unit tests

---
+++ library/Terminal42/ChangeLanguage/PageFinder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Terminal42\ChangeLanguage;
+
+use Contao\Date;
+use Contao\PageModel;
+
+class PageFinder
+{
+    /**
+     * @param PageModel $page
+     *
+     * @return PageModel[]
+     */
+    public function findRootPagesForPage(PageModel $page)
+    {
+        $page->loadDetails();
+
+        $columns = [
+            "type='root'",
+            "(
+                dns=? 
+                OR dns IN (SELECT dns FROM tl_page WHERE type='root' AND fallback='1' AND id IN (SELECT languageRoot FROM tl_page WHERE type='root' AND fallback='1' AND dns=?)) 
+                OR dns IN (SELECT dns FROM tl_page WHERE type='root' AND fallback='1' AND languageRoot IN (SELECT id FROM tl_page WHERE type='root' AND fallback='1' AND dns=?))
+            )"
+        ];
+
+        $this->addPublishingConditions($columns);
+
+        return $this->findPages(
+            $columns,
+            [$page->dns, $page->dns, $page->dns],
+            ['order' => 'sorting']
+        );
+    }
+
+    /**
+     * @param array $columns
+     */
+    private function addPublishingConditions(array &$columns)
+    {
+        if (true !== BE_USER_LOGGED_IN) {
+            $start = Date::floorToMinute();
+            $stop  = $start + 60;
+
+            $columns[] = "published='1'";
+            $columns[] = "(start='' OR start<$start)";
+            $columns[] = "(stop='' OR stop>$stop)";
+        }
+    }
+
+    /**
+     * @param array $columns
+     * @param array $values
+     * @param array $options
+     *
+     * @return \Contao\PageModel[]
+     */
+    private function findPages(array $columns, array $values, array $options = [])
+    {
+        $collection = PageModel::findBy($columns, $values, $options);
+
+        if (null === $collection) {
+            return [];
+        }
+
+        $models = [];
+
+        foreach ($collection as $model) {
+            $models[$model->id] = $model;
+        }
+
+        return $models;
+    }
+}
---
+++ library/Terminal42/ChangeLanguage/Tests/PageFinder/RootPagesTest.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * changelanguage Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2016, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-changelanguage
+ */
+
+namespace Terminal42\ChangeLanguage\Tests\PageFinder;
+
+use Contao\Model\Registry;
+use Contao\PageModel;
+use Terminal42\ChangeLanguage\PageFinder;
+use Terminal42\ChangeLanguage\Tests\ContaoTestCase;
+
+class RootPagesTest extends ContaoTestCase
+{
+    /**
+     * @var PageFinder
+     */
+    private $pageFinder;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->pageFinder = new PageFinder();
+    }
+
+    public function testFindsOneRoot()
+    {
+        $this->createRootPage('', 'en');
+
+        $pageModel = new PageModel();
+        $pageModel->id = 1;
+        $pageModel->dns = '';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 1);
+        $this->assertEquals('root', $roots[1]->type);
+        $this->assertEquals('', $roots[1]->dns);
+        $this->assertEquals('en', $roots[1]->language);
+    }
+
+    public function testFindRootsWithSameDns()
+    {
+        $this->createRootPage('', 'en');
+        $this->createRootPage('', 'de', false);
+
+        $pageModel = new PageModel();
+        $pageModel->id = 1;
+        $pageModel->dns = '';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 2);
+    }
+
+    public function testFindsMasterRoot()
+    {
+        $master = $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('bar.com', 'de', true, $master);
+
+        $pageModel = new PageModel();
+        $pageModel->id = 1;
+        $pageModel->dns = 'foo.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 2);
+    }
+
+    public function testFindsMasterAndNonFallbacks()
+    {
+        $master = $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('foo.com', 'de', false);
+        $this->createRootPage('bar.com', 'fr', true, $master);
+        $this->createRootPage('bar.com', 'it', false);
+
+        $pageModel = new PageModel();
+        $pageModel->id = $master;
+        $pageModel->dns = 'foo.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 4);
+    }
+
+    public function testFindsMasterFromSlave()
+    {
+        $master = $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('foo.com', 'de', false);
+        $this->createRootPage('bar.com', 'fr', true, $master);
+        $search = $this->createRootPage('bar.com', 'it', false);
+
+        $pageModel = new PageModel();
+        $pageModel->id = $search;
+        $pageModel->dns = 'bar.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 4);
+    }
+
+    public function testIgnoresNonRelated()
+    {
+        $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('foo.com', 'de', false);
+        $this->createRootPage('bar.com', 'fr');
+        $this->createRootPage('bar.com', 'it', false);
+
+        $pageModel = new PageModel();
+        $pageModel->id = 1;
+        $pageModel->dns = 'foo.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 2);
+        $this->assertEquals('foo.com', $roots[1]->dns);
+    }
+
+    public function testIgnoresUnpublished()
+    {
+        $master = $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('foo.com', 'de', false);
+        $this->createRootPage('bar.com', 'fr', true, $master);
+        $this->createRootPage('bar.com', 'it', false, 0, false);
+
+        $pageModel = new PageModel();
+        $pageModel->id = 1;
+        $pageModel->dns = 'foo.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 3);
+    }
+
+    public function testNonFallbackMaster()
+    {
+        $master = $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('foo.com', 'de', false);
+        $this->createRootPage('bar.com', 'fr', true, 0);
+        $this->createRootPage('bar.com', 'it', false, $master);
+
+        $pageModel = new PageModel();
+        $pageModel->id = 1;
+        $pageModel->dns = 'foo.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 2);
+    }
+
+    public function testKeyEqualsPageId()
+    {
+        $master = $this->createRootPage('foo.com', 'en');
+        $this->createRootPage('foo.com', 'de', false);
+        $this->createRootPage('bar.com', 'fr', true, $master);
+        $this->createRootPage('bar.com', 'it', false);
+
+        $pageModel = new PageModel();
+        $pageModel->id = $master;
+        $pageModel->dns = 'foo.com';
+
+        $roots = $this->pageFinder->findRootPagesForPage($pageModel);
+
+        $this->assertPageCount($roots, 4);
+
+        foreach ($roots as $id => $page) {
+            $this->assertEquals($page->id, $id);
+        }
+    }
+
+    private function createRootPage($dns, $language, $fallback = true, $master = 0, $published = true)
+    {
+        $fallback  = $fallback ? '1' : '';
+        $published = $published ? '1' : '';
+
+        return $this->query("
+            INSERT INTO tl_page 
+            (type, title, dns, language, fallback, languageRoot, published) 
+            VALUES 
+            ('root', 'foobar', '$dns', '$language', '$fallback', $master, '$published')
+        ");
+    }
+
+    private function assertPageCount($roots, $count)
+    {
+        $this->assertCount($count, $roots);
+
+        foreach ($roots as $instance) {
+            $this->assertInstanceOf('\Contao\PageModel', $instance);
+        }
+    }
+}
