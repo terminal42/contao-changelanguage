4e7d980ddb808dc95a6223ee4b4521ba1b1bd2c7
Andreas Schempp <aschempp@users.noreply.github.com>
2017-02-27 16:38:58 -0300

    Removed author info
    In this case author means for the extension. For individual contributors the Git diff should be enough.

--- library/Terminal42/ChangeLanguage/EventListener/BackendView/AbstractViewListener.php
+++ library/Terminal42/ChangeLanguage/EventListener/BackendView/AbstractViewListener.php
@@ -4,7 +4,6 @@
  *
  * @copyright  Copyright (c) 2008-2016, terminal42 gmbh
  * @author     terminal42 gmbh <info@terminal42.ch>
- * @author     Fritz Michael Gschwantner <fmg@inspiredminds.at>
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  * @link       http://github.com/terminal42/contao-changelanguage
  */
