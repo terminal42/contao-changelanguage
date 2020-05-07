a3a8359332ce765bb238f51161f3a7fb5f5aa203
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-05-07 10:14:19 +0200

    Restructure as Contao 4 bundle

--- config/update.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (0 !== count(array_intersect(ModuleLoader::getActive(), array('newslanguage', 'calendarlanguage')))) {
-    die('changelanguage v3 is not compatible with newslanguage and calendarlanguage. Please remove system/modules/newslanguage and/or system/modules/calendarlanguage');
-}
