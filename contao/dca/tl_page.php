e34463c4518c95b59de2adf8754fb442470eb658
fritzmg <fmg@inspiredminds.at>
2021-09-16 13:53:23 +0100

    do not copy languageMain and languageRoot

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -14,7 +14,7 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['languageMain'] = array
     'label'                   => &$GLOBALS['TL_LANG']['tl_page']['languageMain'],
     'exclude'                 => true,
     'inputType'               => 'pageTree',
-    'eval'                    => array('fieldType'=>'radio', 'multiple'=>false, 'rootNodes'=>[0], 'tl_class'=>'w50 clr'),
+    'eval'                    => array('fieldType'=>'radio', 'multiple'=>false, 'rootNodes'=>[0], 'tl_class'=>'w50 clr', 'doNotCopy' => true),
     'sql'                     => "int(10) unsigned NOT NULL default '0'",
     'load_callback'           => [['Terminal42\ChangeLanguage\EventListener\DataContainer\PageFieldsListener', 'onLoadLanguageMain']],
     'save_callback'           => [['Terminal42\ChangeLanguage\EventListener\DataContainer\PageFieldsListener', 'onSaveLanguageMain']],
@@ -26,7 +26,7 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['languageRoot'] = array
     'exclude'                 => true,
     'inputType'               => 'select',
     'options_callback'        => array('Terminal42\ChangeLanguage\EventListener\DataContainer\PageFieldsListener', 'onLanguageRootOptions'),
-    'eval'                    => array('includeBlankOption'=>true, 'blankOptionLabel'=>&$GLOBALS['TL_LANG']['tl_page']['languageRoot'][2], 'tl_class'=>'w50'),
+    'eval'                    => array('includeBlankOption'=>true, 'blankOptionLabel'=>&$GLOBALS['TL_LANG']['tl_page']['languageRoot'][2], 'tl_class'=>'w50', 'doNotCopy' => true),
     'sql'                     => "int(10) unsigned NOT NULL default '0'"
 );
 
